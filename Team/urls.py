cd9cd080ddeb5489b9998110764fc800c9ef9f1e
Phi Long <philong249@BinMacBookAir.local>
2024-03-08 13:45:57 +0700

    feat(team): add new team

--- Team/urls.py
+++ Team/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path, include
+from django.urls import path
+
 from . import views
 
-
 urlpatterns = [
-    path("league/table", views.LeagueTable, name="League Table"),
+    path("", views.Team, name="League Table"),
     path("league/table/<int:leagueId>/<int:season>", views.LeagueTable, name="League Table by Season"),
     path("league/result/<str:date>", views.LeagueResult, name="League Results from date"),
 ]
