d27264c441f417a4107e74c2bbdcd41514e9427a
PhiLong <reddevils99@nexondv.com>
2024-07-18 17:26:08 +0700

    fix(pylint): bypass too general exception

--- manage.py
+++ manage.py
@@ -16,8 +16,6 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    except:
-        sys.exit()
     execute_from_command_line(sys.argv)
 
 
